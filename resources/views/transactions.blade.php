bf5bd982508cdaf8bf6e6aaab97a7ac95d9adaed
Ansco343 <roger300403@gmail.com>
2026-04-09 15:32:12 +0700

    Update transactions.blade.php

--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -15,19 +15,6 @@
             </tr>
         </thead>
         <tbody class="divide-y">
-            {{-- <tr>
-                <td class="px-6 py-4">01 Mar 2024</td>
-                <td class="px-6 py-4 font-medium">Gaji Bulanan</td>
-                <td class="px-6 py-4"><span class="px-2 py-1 bg-green-100 text-green-700 rounded text-xs">Pemasukan</span></td>
-                <td class="px-6 py-4 text-right text-green-600 font-bold">+ Rp 5.000.000</td>
-            </tr>
-            <tr>
-                <td class="px-6 py-4">02 Mar 2024</td>
-                <td class="px-6 py-4 font-medium">Bayar Kos</td>
-                <td class="px-6 py-4"><span class="px-2 py-1 bg-red-100 text-red-700 rounded text-xs">Tempat Tinggal</span></td>
-                <td class="px-6 py-4 text-right text-red-600 font-bold">- Rp 1.500.000</td>
-            </tr> --}}
-
             @foreach ($transactions as $transaction)
                 <tr>
                 <td class="px-6 py-4">{{$transaction->trans_date}}</td>
